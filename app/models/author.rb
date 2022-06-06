ae3c566ae85802f8b8a9f97036e001f32c84b694
Shantel <snchristian8@gmail.com>
2022-06-06 14:47:16 -0400

    commit tree 23fde4cf9d79517238c88f5912168b8489e8c188

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, uniqueness: true, presence: true
+    validates  :phone_number, length:{is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary,  length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_it_clickable
+
+
+    def is_it_clickable
+        unless title.present? && title.include?("Won't Believe"||"Secret"||"Top [number]"||"Guess")
+            errors.add(:title, "needs to be more clickbait")
+        end
+    end
+
 end
